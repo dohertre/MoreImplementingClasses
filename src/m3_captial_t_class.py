8cd95dcf42b44ee75b44473f341eaa6075a751fc
dohertre <dohertre@rose-hulman.edu>
2019-06-26 15:41:42 -0400

    random

--- src/m3_captial_t_class.py
+++ src/m3_captial_t_class.py
@@ -19,7 +19,7 @@ def main():
     run_test_simple_t()
     run_test_set_colors()
     run_test_move_by()
-    run_test_clone()
+    # run_test_clone()
 
 
 def run_test_simple_t():
@@ -255,7 +255,7 @@ class CapitalT(object):
           :type dy: int
         """
         # --------------------------------------------------------------
-        # TODO: 6.
+        # DONE: 6.
         #   READ the above specification, including the Example.
         #   Implement and test this method by uncommenting the appropriate
         #     run_test method in main. Compare the graphics window to
@@ -286,7 +286,7 @@ class CapitalT(object):
           :rtype: CapitalT
         """
         # --------------------------------------------------------------
-        # TODO: 7.
+        # DONE: 7.
         #   READ the above specification, including the Example.
         #   Implement and test this method by uncommenting the appropriate
         #     run_test method in main. Compare the graphics window to
